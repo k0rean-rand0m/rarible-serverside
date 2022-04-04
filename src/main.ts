07400a9a9570a9f21847e6f86bea3d3b5be5f179
k0rean-rand0m <15913538+k0rean-rand0m@users.noreply.github.com>
2022-04-04 19:51:33 +0800

    Successful initialization of Rarible SDK and an NFT buyout

--- src/main.ts
+++ src/main.ts
@@ -12,15 +12,23 @@ import fetch from "node-fetch"
 
 // Setup
 //@ts-ignore
-global.FormData = FormData
+global.FormData = FormData;
 //@ts-ignore
 global["window"] = { fetch: fetch, dispatchEvent: () => {} }
 //@ts-ignore
 global.CustomEvent = function CustomEvent() { return }
+const provider = new Web3ProviderEngine({ pollingInterval: 100 });
 
-const provider = new Web3ProviderEngine({ pollingInterval: 100 })
-// @ts-ignore
-const wallet = new Wallet(Buffer.from(process.env['ETH_PK'].substring(2), 'hex')); // Wallet private key from ENV variable "ETH_PK"
+if (process.env['ETH_PK'] == undefined ||
+    process.env['ETH_RPC_ENDPOINT'] == undefined ||
+    process.env['BUYOUT_TARGET'] == undefined
+) throw new Error("Provide ETH_PK and ETH_RPC_ENDPOINT as environment variables!");
+
+let privateKey = process.env['ETH_PK'];
+if (privateKey.substring(0, 2) == "0x") {
+    privateKey = privateKey.substring(2);
+}
+const wallet = new Wallet(Buffer.from(privateKey, 'hex')); // Wallet private key from ENV variable "ETH_PK"
 provider.addProvider(new TestSubprovider(wallet, { networkId: 4, chainId: 4 })); // Rinkeby Network (id 4)
 provider.addProvider(new RpcSubprovider({ rpcUrl: process.env['ETH_RPC_ENDPOINT'] })); // RPC http endpoint from ENV variable "ETH_RPC_ENDPOINT"
 provider.start();
@@ -34,13 +42,15 @@ const raribleSdkWallet = new EthereumWallet(raribleProvider);
 const raribleSdk = createRaribleSdk(raribleSdkWallet, 'staging', { fetchApi: fetch });
 
 // Buy function
-async function buy(item: string): Promise<any> {
+async function buy(item: string) {
     // item is token_address:token_id
     const order = (await raribleSdk.apis.item.getItemById({ itemId: 'ETHEREUM:' + item })).bestSellOrder;
     if (order) {
         try {
             const request = await raribleSdk.order.buy({ orderId: order.id });
-            return await request.submit({ amount: 1 });
+            console.log("The transaction was sent, waiting for a Rarible Protocol response...");
+            const response = await request.submit({ amount: 1 });
+            console.log("Rarible Protocol response:", response);
         } catch (e) {
             console.log('Error', e);
         }
@@ -48,5 +58,4 @@ async function buy(item: string): Promise<any> {
 }
 
 // Buying 1 item of https://rinkeby.rarible.com/token/0x1AF7A7555263F275433c6Bb0b8FdCD231F89B1D7:18661571940073987827662103527955627190048515004732602540856362757661044768826?tab=owners
-const raribleBuyResponse = buy("0x1AF7A7555263F275433c6Bb0b8FdCD231F89B1D7:18661571940073987827662103527955627190048515004732602540856362757661044768826")
-console.log(raribleBuyResponse)
+buy(process.env['BUYOUT_TARGET']).then(_ => {});
